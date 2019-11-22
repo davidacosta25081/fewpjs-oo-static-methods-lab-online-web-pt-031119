39669cc7a8dc3a821f6a80ba65d869d61357939b
David Acosta <davidacosta25081@yahoo.com>
2019-11-22 22:35:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 class Formatter {
 
-static capitalize(word) {
-  return word.charAt(0).toUpperCase() + word.slice(1)
-}
+  static capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  }
 
-static sanitize(word) {
-  return word.replace(/[^A-Za-z0-9-']+/g, '');
+  static sanitize(word) {
+    return word.replace(/[^A-Za-z0-9-']+/g, '');
+  }
 
 }
